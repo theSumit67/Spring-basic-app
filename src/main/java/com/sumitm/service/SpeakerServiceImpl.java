4e0c23a5b370fe05c2f39133401991a556a2ad5b
Sumit M <thesumit67@gmail.com>
2020-12-07 13:21:34 +0530

    @Autowired setter injection

--- src/main/java/com/sumitm/service/SpeakerServiceImpl.java
+++ src/main/java/com/sumitm/service/SpeakerServiceImpl.java
@@ -2,6 +2,9 @@ package com.sumitm.service;
 
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.sumitm.repository.HibernateSpeakerRepositoryImpl;
 import com.sumitm.repository.SpeakerRepository;
 import com.sumitm.model.Speaker;
@@ -10,15 +13,21 @@ public class SpeakerServiceImpl implements SpeakerService {
 
 	// Hard coded, can be solved using spring
 	private SpeakerRepository repository; // = new HibernateSpeakerRepositoryImpl();
-	
+
+	public SpeakerServiceImpl () {
+		System.out.println("SpeakerServiceImpl no args constructor");
+	}
+
 	public SpeakerServiceImpl (SpeakerRepository speakerRepository) {
+		System.out.println("SpeakerServiceImpl speakerRepository constructor");
 		repository = speakerRepository;
 	}
 
 	public List<Speaker> findAll(){
 		return repository.findAll();
 	}
-
+	
+	@Autowired
 	public void setRepository(SpeakerRepository repository) {
 		this.repository = repository;
 	}	
