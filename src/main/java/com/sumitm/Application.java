f4b23642d2791c6a0b5b48500f028068ef73d8ff
Sumit M <thesumit67@gmail.com>
2020-12-07 17:03:13 +0530

    Spring app with XML config

--- src/main/java/com/sumitm/Application.java
+++ src/main/java/com/sumitm/Application.java
@@ -1,12 +1,19 @@
 package com.sumitm;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.sumitm.service.SpeakerService;
 import com.sumitm.service.SpeakerServiceImpl;
 
 public class Application {
 
 	public static void main(String[] args) {
-		SpeakerService service = new SpeakerServiceImpl();
+		
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+//		SpeakerService service = new SpeakerServiceImpl();
+		SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
 		System.out.println(service.findAll().get(0).getFirstName());
 	}
 
--- src/main/java/com/sumitm/service/SpeakerServiceImpl.java
+++ src/main/java/com/sumitm/service/SpeakerServiceImpl.java
@@ -2,16 +2,31 @@ package com.sumitm.service;
 
 
 import java.util.List;
-import com.sumitm.repository.HibernateSpeakerRepositoryImpl;
 import com.sumitm.repository.SpeakerRepository;
 import com.sumitm.model.Speaker;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
 	// Hard coded, can be solved using spring
-	private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+	private SpeakerRepository repository; // = new HibernateSpeakerRepositoryImpl();
 	
+	// default constructor required for auto wiring byType
+	public SpeakerServiceImpl() {
+
+	}
+
+	public SpeakerServiceImpl(SpeakerRepository repository) {
+		this.repository = repository;
+	}
+
 	public List<Speaker> findAll(){
 		return repository.findAll();
 	}
+
+	// matches this name to xml bean config 
+	// when auto wiring this method gets called automatically, since it has SpeakerRepository as property, so it matches name setSpeakerRepository
+	public void setSpeakerRepository(SpeakerRepository repository) {
+		this.repository = repository;
+	}
+
 }
